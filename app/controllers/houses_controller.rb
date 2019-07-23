65ca11ae91cf64050818f4a592e88b9028b28c99
Ryota Suzuki <ryotasuzuki.com@gmail.com>
2019-07-23 16:35:59 +0000

    add lm

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -48,8 +48,8 @@ class HousesController < ApplicationController
     end
   end
   
-  def lm(x1,x2)
-    100 + x1*100 + x2*200
+  def lm(x1,x2,x3,x4,x5)
+   (27073 + 4.82355928*x1 - 1097.43589744*x2 + 1625.64102564*x3  - 48.23559279*x4 - 2076.92307692*x5).to_i
   end
 
   private
